9bbe9cab7dde1e15e0269289841af2747f95df29
choudel <chouaibh@gmail.com>
2022-08-24 15:37:19 +0200

    controllers methods

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Delete, Get, Param, Post, Put } from "@nestjs/common";
+import { Controller, Delete, Get, Param, Post, Put, Body } from "@nestjs/common";
+import { report } from "process";
 import {data, ReportType} from 'src/data';
+import { v4 as uuid } from "uuid";
 
 @Controller('report/:type')
 export class AppController {
@@ -16,15 +18,48 @@ export class AppController {
         return data.report.filter((report)=>report.type===reportType).find(report=>report.id===id)
     }
     @Post()
-    createIncomeReport(){
-        return 'created'
+    createIncomeReport(
+        @Body(){source,amount}:{
+            amount:number,
+            source:string
+        },@Param('type')type:string
+    ){
+        const newReport={
+            id:uuid(),
+            source,
+            amount,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type : type==='income' ? ReportType.INCOME : ReportType.EXPENSE
+        }
+        data.report.push(newReport)
+        return newReport
     }
     @Put(':id')
-    updateIncomeReport(){
-        return 'updated'
+    updateIncomeReport( 
+       @Param('type')type:string,
+       @Param('id')id:string,
+       @Body()body:{
+        amount:number,
+        source:string}
+    ){
+        const reportType=
+            type==='income'?ReportType.INCOME :ReportType.EXPENSE
+        const reportToUpdate = data.report.filter((report)=>report.type===reportType).find((report)=> report.id===id)
+        if(!reportToUpdate)return
+
+        const reportIndex =data.report.findIndex((report)=>report.id===reportToUpdate.id)
+
+        data.report[reportIndex]={
+            ...data.report[reportIndex],
+            ...body,
+        }
+        return data.report[reportIndex]
     }
     @Delete(':id')
-    deleteIncomeReport(){
-        return 'deleted'
+    deleteIncomeReport(@Param('id') id:string){
+        const reportIndex=data.report.findIndex(report=>report.id===report.id)
+        if(reportIndex===-1)return
+        data.report.splice(reportIndex,1)
     }
 }
